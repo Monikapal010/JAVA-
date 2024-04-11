ce14eb55b9a711ef921df9c67b39150c067458e0
Monikapal010 <2022bit010@sggs.ac.in>
2024-04-11 21:20:03 +0530

    work of random

---
+++ random.java
@@ -0,0 +1,17 @@
+class Random{
+ 
+ public static void main(String args[])
+ {
+  Random r=new Random();
+  r.getRandomDigit();
+ }
+ public  void getRandomDigit()
+ {
+ long digit=System.nanoTime();
+ return digit;
+  
+ //System.out.println(System.nanoTime());
+ //return System.nano.Time;
+ 
+}
+}
