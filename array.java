85e5a0731b4ea9f51ff31cc17c1ec244179a99a5
Monikapal010 <2022bit010@sggs.ac.in>
2024-04-14 03:15:09 +0530

    work

---
+++ array.java
@@ -0,0 +1,31 @@
+class string{
+public static void main(String[] args)
+{
+/*String s="sggs";
+String s=new String("hello");
+System.out.println(s);
+
+char a[]={'m','o','n','i','k','a'};
+String m=new String(a);
+System.out.println(m);
+if(s==m)
+{
+System.out.println("both are same");
+
+}
+else {
+System.out.println("not same");
+*/
+String s1="hello";
+String s2=new String("hello");
+StringBuffer sb=new StringBuffer("hello");
+if(s1.equals(s2))
+{
+System.out.println("both are same");
+
+}
+else {
+System.out.println("not same");
+}
+}
+}
