7cd17e5e7a84ecd0a106e7843dec6b3b97431077
Monikapal010 <2022bit010@sggs.ac.in>
2024-04-16 04:02:47 +0530

    variable commit

---
+++ variable.java
@@ -0,0 +1,13 @@
+    class variable
+    {  
+        static int m=30;
+        
+        void method()  
+        {    
+            int n=90;   
+        }  
+        public static void main(String args[])  
+        {  
+            int d=50;    
+        }  
+    }
