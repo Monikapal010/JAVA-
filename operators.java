825fd9d3ec08c22f04fe52066478f834767b04cb
Monikapal010 <2022bit010@sggs.ac.in>
2024-04-18 00:53:52 +0530

    operators

---
+++ operators.java
@@ -0,0 +1,25 @@
+class operators{
+
+static int a=30;
+static int b=20;
+
+
+public static void main(String[] args)
+{
+System.out.println(a++);
+System.out.println(a++);
+System.out.println(++a);
+System.out.println(--a);
+System.out.println(a);
+System.out.println(a--);
+System.out.println(a++ + a++);
+System.out.println(a-- - --a);
+System.out.println(10>>3);
+System.out.println(10<<4);
+
+
+
+
+}
+}
+
