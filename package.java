2c650e2f68baf152db76f4d444a8e0116674db34
Monikapal010 <2022bit010@sggs.ac.in>
2024-05-08 02:00:49 +0530

    work

---
+++ package.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class PackageExample{
+
+	public static void main(String args[]){
+	        String Input="monikaaa";
+	        Scanner sc=new Scanner(System.in);
+	         Scanner bb=new Scanner(Input);
+		System.out.println("aaa");
+		}
+		// public static void gc();
+
+		
+		//Scanner sc=new Scanner();
+	               
+                   }
