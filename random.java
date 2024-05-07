d7f052f5879decbc90dc30577827f9286d58a82c
Monikapal010 <2022bit010@sggs.ac.in>
2024-05-07 22:46:50 +0530

    work

--- random.java
+++ random.java
@@ -1,17 +1,58 @@
 class Random{
  
- public static void main(String args[])
- {
-  Random r=new Random();
-  r.getRandomDigit();
- }
- public  void getRandomDigit()
- {
- long digit=System.nanoTime();
- return digit;
-  
- //System.out.println(System.nanoTime());
- //return System.nano.Time;
  
+public static void main(String args[])
+    {
+	  Random r=new Random();
+	  r.getRandomDigit();
+	  long randomNumber=getRandomDigit();
+	  long startTime=System.nanoTime();
+	  long endTime=System.nanoTime();
+	  long timeTaken=endTime-startTime;
+	  
+	  System.out.println("Random number:"+randomNumber);
+	  System.out.println("start Time:"+startTime);
+	  System.out.println("end Time:"+endTime);
+	  System.out.println("time taken:"+timeTaken+"seconds");
+	  
+	  
+     }
+ public static  long getRandomDigit()
+ {
+	 long digit=System.nanoTime();
+	 return digit;
+ } 
+ public long Number(){
+     
+    long N=0;
+    for(N=0;N<7;N++){
+    
+    long digit=getRandomDigit();
+    
+    }
+    return N;
+ 
+ }
+ public boolean checkPrime(long n){
+ 
+  for(int i=2;i*i<=n;i++){
+  
+	if(n%i==0){
+	  
+	  return false;
+	  
+        }
+        else{
+        
+        return true;
+        
+        } 
+    }
+        return true; 
+
+     }
+   
+   
 }
-}
+	
+
